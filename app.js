bb382020f2c65d319053c7431c59806d8901433c
CARLRAM2482 <145763670+CARLRAM2482@users.noreply.github.com>
2024-09-17 21:49:45 -0500

    v2.2 clave

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 let  cantidad = document.getElementById('cantidad');
 let boton = document.getElementById('crear');
-
+let clave =  document.getElementById('clave');
 
 
 //caracteres para generar la contraseña
-const cadenaCaracteres = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789'
+const cadenaCaracteres = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789.,+@!¡'
 
 
 // funciones
 function crear(){
 
-    let numeroDigitado = parseInt (cantidad.Value);
+    let numeroDigitado = parseInt (cantidad.value);
     console.log(numeroDigitado);
 
     if(numeroDigitado < 8){
         alert("la cantidad de caracteres mayor que 10 ");
         }
-}
+
+
+let contraseña = '';
+
 //condiciones.
-for(let i=0; i < numeroDigitado; i++){
-    let indiceAleatorio = Math.random() * cadenaCaracteres.length;
+for(let i= 0; i < numeroDigitado; i++){
+
+    let indiceAleatorio = cadenaCaracteres[Math.floor( Math.random() * cadenaCaracteres.length)];
+    console.log(cadenaCaracteres);
+
+    contraseña+=indiceAleatorio;
 }
 
-
+clave.value = contraseña;
+}
